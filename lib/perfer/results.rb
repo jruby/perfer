4bd670097e72672832ecb5af157da1f15fb851a3
Benoit Daloze <eregontp@gmail.com>
2012-07-05 20:34:59 +0200

    add a Results class to aggregate the measurements

---
+++ lib/perfer/results.rb
@@ -0,0 +1,29 @@
+require 'forwardable'
+
+module Perfer
+  # A set of results for a particular job
+  class Results
+    extend Forwardable
+
+    def initialize
+      @results = []
+    end
+
+    delegate [:<<, :size, :length, :to_a] => :@results
+
+    def aggregate
+      stats = Statistics.new(on(:real))
+      mean, stddev = stats.mean, stats.stddev
+      {
+        :mean => mean,
+        :stddev => stddev,
+        :stddev3 => 3*stddev,
+        :percent_incertitude => (3*stddev / mean * 100)
+      }
+    end
+
+    def on(field)
+      @results.map { |result| result[field] }
+    end
+  end
+end

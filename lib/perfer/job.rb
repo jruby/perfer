4d5ed65a1f66fcc936077d79a0cf67c83a48bc25
Benoit Daloze <eregontp@gmail.com>
2012-07-05 23:13:27 +0200

    eager-initialize instead of lazy-initialize Job's metadata

--- lib/perfer/job.rb
+++ lib/perfer/job.rb
@@ -2,21 +2,19 @@ module Perfer
   class Job
     MINIMAL_TIME = 1.0 # 0.2
 
-    attr_reader :session, :title, :results
+    attr_reader :session, :title, :results, :metadata
     def initialize(session, title, &block)
       @session = session
       @title = title
       @block = block
       @results = Results.new
+
+      # TODO: add file, checksum
+      @metadata = { :title => @session.title, :job => @title }.freeze
     end
 
     def measurements
       10
-    end
-
-    def metadata
-      # TODO: add file, checksum
-      @metadata ||= { :title => @session.title, :job => title }.freeze
     end
   end
 end

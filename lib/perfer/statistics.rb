a167b7c5e51ccaeea8664be47343ce9c8a55ecbf
Benoit Daloze <eregontp@gmail.com>
2012-07-05 20:32:18 +0200

    add a Statistics class to analyze results
    * a class is used, so caching is handled transparently and the interface is clean and clear

---
+++ lib/perfer/statistics.rb
@@ -0,0 +1,25 @@
+module Perfer
+  class Statistics
+    attr_reader :sample, :size
+    def initialize(sample)
+      @sample = sample
+      @size = sample.size
+    end
+
+    def mean
+      @mean ||= @sample.inject(0.0) { |sum, i| sum + i } / @size
+    end
+
+    def variance
+      mean = mean()
+      @sample.inject(0.0) { |var, i|
+        d = i - mean
+        var + d*d
+      } / @size
+    end
+
+    def stddev
+      Math.sqrt(variance)
+    end
+  end
+end

4bf08216374222d13c98c83a702fb68c4e15bd4b
Benoit Daloze <eregontp@gmail.com>
2012-07-17 14:23:24 +0200

    revamp CLI and add help, better error messages and exit statuses

--- lib/perfer/cli.rb
+++ lib/perfer/cli.rb
@@ -2,84 +2,128 @@ module Perfer
   module CLI
     COMMANDS = %w[
       config
+      help
       report
       results
       run
     ]
 
+    HELP = <<-EOS
+Usage:
+  perfer <command> [options] arguments
+
+Commands:
+  run files+    - run with current ruby
+  report files+ - show the results
+  config reset  - reset the configuration file to the defaults (or create it)
+  help          - show this help
+  results
+    path files+      - show the paths to the result files
+    rm,delete files+ - remove the result files
+
+<files+> are a set of benchmark files
+
+Common options:
+EOS
+
     class << self
       def execute(argv)
-        command = argv.shift
-        unless command
-          raise Error, "A command must be given, one of: #{COMMANDS*', '}"
-        end
-        unless COMMANDS.include?(command)
-          raise Error, "Unknown command: #{command.inspect}"
-        end
-
-        OptionParser.new do |options|
+        @opts = OptionParser.new do |options|
+          options.banner = HELP
           common_options(options)
-        end.parse!(argv)
+        end
 
-        send(command, argv)
+        begin
+          @opts.parse!(argv)
+        rescue OptionParser::ParseError => e
+          error e.message
+        end
+
+        @command = argv.shift
+        error "A command must be given, one of: #{COMMANDS*', '}" unless @command
+        error "Unknown command: #{command.inspect}" unless COMMANDS.include? @command
+
+        send(@command, *argv)
       end
 
-      def report(argv)
-        load_files(argv)
+      def unknown_subcommand(subcommand)
+        if subcommand
+          error "Unknown subcommand for #{@command}: #{subcommand.inspect}"
+        else
+          error "`perfer #{@command}` needs a subcommand"
+        end
+      end
+
+      def error message
+        $stderr.puts message
+        $stderr.puts
+        abort @opts.help
+      end
+
+      def help
+        puts @opts.help
+      end
+
+      def report(*files)
+        load_files(files)
         sessions.each { |session|
           session.report
         }
       end
 
-      def run(argv)
-        load_files(argv)
+      def run(*files)
+        load_files(files)
         sessions.each(&:run)
       end
 
-      def results(argv)
-        case subcommand = argv.shift
+      def results(*files)
+        case subcommand = files.shift
         when "path"
-          load_files(argv)
+          load_files(files)
           sessions.each { |session|
             puts session.store.file
           }
         when "delete", "rm"
-          load_files(argv)
+          load_files(files)
           sessions.each { |session|
             session.store.delete
           }
         when "rewrite"
-          load_files(argv)
+          load_files(files)
           sessions.each { |session|
             session.store.rewrite
           }
         else
-          raise Error, "Unknown subcommand: #{subcommand}"
+          unknown_subcommand subcommand
         end
       end
 
-      def config(argv)
-        case subcommand = argv.shift
+      def config(*files)
+        case subcommand = files.shift
         when "reset"
           Perfer.configuration.write_defaults
         else
-          raise Error, "Unknown subcommand: #{subcommand}"
+          unknown_subcommand subcommand
         end
       end
 
       def common_options(options)
         options.on('-t TIME', Float, "Minimal time for to run (greater usually improve accuracy)") do |t|
-          raise Error, "Minimal time must be > 0" if t <= 0
+          error "Minimal time must be > 0" if t <= 0
           Perfer.configuration.minimal_time = t
         end
         options.on('-m N', Integer, "Numbers of measurements per job") do |n|
-          raise Error, "There must be at least 2 measurements" if n < 2
+          error "There must be at least 2 measurements" if n < 2
           Perfer.configuration.measurements = n
+        end
+        options.on('-h', '--help', "Show this help") do
+          puts options.help
+          exit
         end
       end
 
-      def load_files(argv)
-        argv.each do |file|
+      def load_files(files)
+        files.each do |file|
           require File.expand_path(file)
         end
       end

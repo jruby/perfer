13730b7b62f6582e582af4580192249da9d557e3
Benoit Daloze <eregontp@gmail.com>
2012-08-10 00:49:46 +0200

    improve spec for Platform.command_line

--- spec/perfer/platform/command_line_spec.rb
+++ spec/perfer/platform/command_line_spec.rb
@@ -2,6 +2,6 @@ require 'spec_helper'
 
 describe "Perfer::Platform.command_line" do
   it 'gives back the command line that launched this process' do
-    Perfer::Platform.command_line.should match(/ruby|jruby|rbx/)
+    Perfer::Platform.command_line.should match(/(?:j?ruby|rbx) /)
   end
 end

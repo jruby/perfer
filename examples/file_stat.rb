9adbc60fa5d332d0921726c73dbe8a272cb16a13
Benoit Daloze <eregontp@gmail.com>
2012-08-10 19:01:48 +0200

    update the File.stat example to use the eval API
    MRI:
    File.stat BLOCK 6.159 µs/i ±  0.5% <=> 162370 ips
    File.stat EVAL  5.748 µs/i ±  0.3% <=> 173965 ips
    
    File.stat BLOCK 6.263 µs/i ±  0.4% <=> 159666 ips
    File.stat EVAL  5.842 µs/i ±  0.5% <=> 171165 ips
    
    File.stat BLOCK 6.304 µs/i ±  0.2% <=> 158620 ips
    File.stat EVAL  5.861 µs/i ±  0.3% <=> 170619 ips
    
    JRuby:
    File.stat  7.911 µs/i ±  0.2% <=> 126410 ips
    File.ctime 8.057 µs/i ±  0.3% <=> 124117 ips
    File.mtime 8.157 µs/i ±  0.9% <=> 122597 ips
    =>
    File.stat  7.143 µs/i ±  0.9% <=> 140000 ips
    File.ctime 7.295 µs/i ±  0.8% <=> 137078 ips
    File.mtime 7.626 µs/i ±  4.7% <=> 131134 ips

--- examples/file_stat.rb
+++ examples/file_stat.rb
@@ -2,15 +2,7 @@ require 'tmpdir'
 tmpdir = Dir.tmpdir
 
 Perfer.session "File.stat" do |b|
-  b.iterate "File.stat" do
-    File.stat(tmpdir)
-  end
-
-  b.iterate "File.ctime" do
-    File.ctime(tmpdir)
-  end
-
-  b.iterate "File.mtime" do
-    File.mtime(tmpdir)
-  end
+  b.iterate "File.stat",  "File.stat(dir)",  :dir => tmpdir
+  b.iterate "File.ctime", "File.ctime(dir)", :dir => tmpdir
+  b.iterate "File.mtime", "File.mtime(dir)", :dir => tmpdir
 end

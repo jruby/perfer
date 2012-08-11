b0c186b67be9b8e5e33dc69b00dee0e3702e33fe
Benoit Daloze <eregontp@gmail.com>
2012-08-11 21:19:38 +0200

    avoid empty blocks/code in examples/iterate_overhead.rb
    * still not accurate, "0" is just a simple literal

--- examples/iterate_overhead.rb
+++ examples/iterate_overhead.rb
@@ -1,13 +1,15 @@
 Perfer.session "#iterate overhead" do |b|
   b.iterate "Simple block" do
+    0
   end
 
   b.iterate "Block with given argument" do |n|
     i = 0
     while i < n
+      0
       i += 1
     end
   end
 
-  b.iterate "String for eval", ""
+  b.iterate "String for eval", "0"
 end

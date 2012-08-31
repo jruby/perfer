ab1d14c68bf964b508752dff196ad8047f189eef
Benoit Daloze <eregontp@gmail.com>
2012-08-31 11:31:11 +0200

    avoid allocating lots of memory in the memory spec, expect OS functions to be right
    * speed up tests by ~0.2s

--- spec/perfer/platform/memory_spec.rb
+++ spec/perfer/platform/memory_spec.rb
@@ -9,15 +9,7 @@ describe "Perfer::Platform.{maximum_,}memory_used" do
   end
 
   it 'reports the maximum memory used until now and the current memory usage' do
-    (1..150).should include memory_used
-    (1..150).should include max_memory_used
-
-    # allocate 100MB
-    a = Array.new(100*1024*1024/0.size, 0)
-
-    (101..250).should include memory_used
-    (101..250).should include max_memory_used
-
-    a.size
+    (1..200).should include memory_used
+    (1..200).should include max_memory_used
   end
 end

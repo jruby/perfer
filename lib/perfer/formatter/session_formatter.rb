d885c1ca6d39c2b1a3e4c1898abde20d7a2542ea
Benoit Daloze <eregontp@gmail.com>
2012-09-26 16:39:34 +0200

    show when the benchmark file changed in report's output

--- lib/perfer/formatter/session_formatter.rb
+++ lib/perfer/formatter/session_formatter.rb
@@ -10,10 +10,17 @@ module Perfer
       return puts "No results available." unless @session.results
       session_name = @session.results.first[:session]
       puts session_name
+      last_bench_file_checksum = @session.results.first[:bench_file_checksum]
       @session.results.chunk { |r|
         r[:run_time]
       }.each { |run_time, results|
-        puts "Ran at #{format_time run_time} with #{results.first[:ruby]}"
+        result = results.first
+        if last_bench_file_checksum != result[:bench_file_checksum]
+          puts "-- The benchmark script changed --"
+          puts
+          last_bench_file_checksum = result[:bench_file_checksum]
+        end
+        puts "Ran at #{format_time run_time} with #{result[:ruby]}"
         ResultsFormatter.new(results).report(options)
         puts
       }

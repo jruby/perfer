e8aca1b5f3f6dbcc1a4b4d0f5ab71e1a29b426af
Benoit Daloze <eregontp@gmail.com>
2013-10-03 17:35:26 +0200

    remove tests related to normal distribution

--- spec/perfer/statistics_spec.rb
+++ spec/perfer/statistics_spec.rb
@@ -15,13 +15,5 @@ describe Perfer::Statistics do
   its(:standard_error)            { should be_within(1e-9).of 0.001428471 }
   its(:mean_absolute_deviation)   { should be_within(1e-8).of 0.00277964 }
   its(:median_absolute_deviation) { should be_within(1e-7).of 0.0008845 }
-  its(:margin_of_error)           { should be_within(1e-9).of 0.003231202 }
   its(:maximum_absolute_deviation){ should be_within(1e-7).of 0.0120181 }
-
-  it "confidence interval test" do
-    d = [19.3, 20.1, 20.4, 19.6, 19.1, 20.2, 19.8, 20.3, 20.1, 19.6]
-    s = Perfer::Statistics.new(d)
-    (s.mean - s.margin_of_error).should be_within(1e-2).of 19.53
-    (s.mean + s.margin_of_error).should be_within(1e-2).of 20.17
-  end
 end
